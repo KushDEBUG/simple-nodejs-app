0d51027d70ea831ee3dfb33eb8b544641adebbe5
Kushagra Mishra <144323330+KushDEBUG@users.noreply.github.com>
2026-07-10 11:36:15 +0000

    Fix fixable vulnerabilities: remove request, upgrade express/ejs/nodemon

--- index.js
+++ index.js
@@ -1,53 +1,43 @@
 const express = require('express');
 const app = express();
-const request = require('request');
 const wikip = require('wiki-infobox-parser');
 
-//ejs
 app.set("view engine", 'ejs');
 
-//routes
-app.get('/', (req,res) =>{
+app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.get('/index', (req,response) =>{
-    let url = "https://en.wikipedia.org/w/api.php"
+app.get('/index', async (req, response) => {
+    let url = "https://en.wikipedia.org/w/api.php";
     let params = {
         action: "opensearch",
         search: req.query.person,
         limit: "1",
         namespace: "0",
         format: "json"
+    };
+    url = url + "?";
+    Object.keys(params).forEach((key) => {
+        url += '&' + key + '=' + params[key];
+    });
+
+    try {
+        const res = await fetch(url);
+        const result = await res.json();
+        let x = result[3][0];
+        x = x.substring(30, x.length);
+
+        wikip(x, (err, final) => {
+            if (err) {
+                response.redirect('404');
+            } else {
+                response.send(final);
+            }
+        });
+    } catch (err) {
+        response.redirect('404');
     }
-
-    url = url + "?"
-    Object.keys(params).forEach( (key) => {
-        url += '&' + key + '=' + params[key]; 
-    });
-
-    //get wikip search string
-    request(url,(err,res, body) =>{
-        if(err) {
-            response.redirect('404');
-        }
-            result = JSON.parse(body);
-            x = result[3][0];
-            x = x.substring(30, x.length); 
-            //get wikip json
-            wikip(x , (err, final) => {
-                if (err){
-                    response.redirect('404');
-                }
-                else{
-                    const answers = final;
-                    response.send(answers);
-                }
-            });
-    });
-
-    
 });
 
-//port
-app.listen(3000, console.log("Listening at port 3000..."))
+app.listen(3000, console.log("Listening at port 3000..."));
